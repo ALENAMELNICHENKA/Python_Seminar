e6b82fbc0e4d9c499b906841f24362fc4b73387b
Alena Melnichenka <6983314@mail.ru>
2024-01-23 14:53:52 +0300

    Лекция 2. Коллекции данных

---
+++ Lectures/2.py/L2.2.py
@@ -0,0 +1,9 @@
+# Метод pop удаляет последний элемент из списка:
+
+list_1 = [12, 7, -1, 21, 0] 
+print(list_1.pop())# 0 
+print(list_1)# [12, 7, -1, 21]
+print(list_1.pop())# 21 
+print(list_1)# [12, 7, -1] 
+print(list_1.pop())# -1 
+print(list_1)# [12, 7]
